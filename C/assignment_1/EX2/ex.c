aa005d5ea4497faf7aff08dcddd9d455dd69be8c
Ahmed Zaki <121236654+AhmedZakiAmer@users.noreply.github.com>
2025-01-22 23:10:35 +0200

    Update ex.c

--- C/assignment_1/EX2/ex.c
+++ C/assignment_1/EX2/ex.c
@@ -10,15 +10,20 @@
 
 int main(void){
 	char alpha ;
+	int flag = 0;
 	printf("Enter an alphabet: ");
 	fflush(stdout);
 	scanf("%c",&alpha);
 	if (alpha >= 'A' && alpha <= 'Z'){
 		alpha += 32;
+		flag = 1;
 	}
 	if (alpha == 'a' || alpha == 'e' || alpha == 'i' || alpha == 'o' || alpha == 'u'){
 		printf("%c is a vowel.",alpha);
 	}else {
+		if(flag){
+			alpha -= 32;
+		}
 		printf("%c is a consonant.",alpha);
 	}
 
