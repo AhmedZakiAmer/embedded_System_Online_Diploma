2415bd8d7dfff69f3f373987558062aaa62d19dc
Ahmed Zaki <mido569zaki@gmail.com>
2025-01-27 14:40:38 +0200

    add 1st fn

---
+++ C/assignment_4/Ex1/ex1.c
@@ -0,0 +1,17 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Jan 25, 2025
+ *      Author: ahmed
+ */
+
+#include "stdio.h"
+
+
+
+int main(void){
+	setvbuf(stdout,NULL,_IONBF,0);
+
+
+	return 0;
+}
---
+++ C/quiz codes/4th_least_significant/significant_bit.c
@@ -0,0 +1,18 @@
+/*
+ * significant_bit.c
+ *
+ *  Created on: Jan 27, 2025
+ *      Author: ahmed
+ */
+
+#include "stdio.h"
+
+int main(void){
+	setvbuf(stdout,NULL,_IONBF,0);
+	int num,bit;
+	printf("Enter the number: ");
+	scanf("%d",&num);
+	bit = (num>>3)&1;
+	printf("%d",bit);
+	return 0;
+}
