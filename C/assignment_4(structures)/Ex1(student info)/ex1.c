4d5ffc882e854401649c5f60f297836aec6d309b
Ahmed Zaki <mido569zaki@gmail.com>
2025-01-28 16:23:55 +0200

    add 1st EX

---
+++ C/assignment_4(structures)/Ex1(student info)/ex1.c
@@ -0,0 +1,33 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Jan 28, 2025
+ *      Author: ahmed
+ */
+
+#include "stdio.h"
+#include "string.h"
+
+typedef struct{
+	char name[20];
+	int roll;
+	float marks;
+}Students;
+
+int main(void){
+	setvbuf(stdout,NULL,_IONBF,0);
+	Students student;
+	printf("Enter info of student\n");
+	printf("Enter name: ");
+	scanf("%s",student.name);
+	printf("Enter roll number: ");
+	scanf("%d",&student.roll);
+	printf("Enter marks: ");
+	scanf("%f",&student.marks);
+	printf("displaying info\n");
+	printf("name: %s\n",student.name);
+	printf("name: %d\n",student.roll);
+	printf("name: %.2f\n",student.marks);
+
+	return 0;
+}
